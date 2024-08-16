4e97fa5e2a453d0539faf9f06b651c6bdef511a7
florian <florianlima51@gmail.com>
2024-08-16 19:46:20 +0200

    déclaration polices & décharge correction

--- chicdressing/functions.php
+++ chicdressing/functions.php
@@ -9,8 +9,16 @@ function chicdressing_enqueue_styles()
 add_filter('big_image_size_threshold', '__return_false');
 
 function remove_google_fonts_stylesheet() {  
-    wp_dequeue_style( 'ashe_playfair_font' );
-    wp_dequeue_style( 'ashe_opensans_font' );
-    wp_dequeue_style( 'ashe_kalam_font' );
+    wp_dequeue_style( 'ashe-playfair-font' );
+    wp_dequeue_style( 'ashe-opensans-font' );
+    wp_dequeue_style( 'ashe-kalam-font' );
 }
 add_action( 'wp_enqueue_scripts', 'remove_google_fonts_stylesheet', 999 );
+
+/**remove_action( 'woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title', 10 );
+add_action('woocommerce_shop_loop_item_title', 'wps_change_products_title', 10 );
+function wps_change_products_title() {
+    echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '">' . get_the_title() . '</h3>';
+}*/
+
+?>
